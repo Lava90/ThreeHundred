31fe6140aad384980b26fb5e501e93efd23d5e5d
Lava90 <lava1990@163.com>
2015-12-24 14:34:27 +0800

    028、029

--- programing/Procedure028.java
+++ programing/Procedure028.java
@@ -1,14 +1,30 @@
 package com.lava.procedure;
-
+//   *   
+//  * *  
+// *   * 
+//*     *
+// *   * 
+//  * *  
+//   *  
 public class Procedure028 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int sum=8;
-		int k=4;
+		int size=4;
 		for(int i=1;i<=7;i++){
+			// TODO Auto-generated method stub
 			for(int j=1;j<=7;j++){
-				
+				if(j==size||j==8-size){
+					System.out.print("*");
+				}else{
+					System.out.print(" ");
+				}				
+			}
+			System.out.println();
+			
+			if(i>=4){
+				size++;
+			}else{
+				size--;
 			}
 		}
 	}
